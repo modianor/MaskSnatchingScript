bb7d9e1b213bf5de2355319a4ecc3f03a25178c0
modianor <1138716463@qq.com>
2020-02-07 22:09:49 +0800

    ...

--- constant.py
+++ constant.py
@@ -3,17 +3,17 @@
 PARAMS_MASK = {
     'goods_attr_id': '653',  # 药店ID
     'num': '1',  # 购买数量
-    'author': '徐嘉伟',  # 购买人姓名
-    'tel': '18100695026',  # 电话号码
+    'author': '',  # 购买人姓名
+    'tel': '',  # 电话号码
     'fid': '1850',  # 商品ID
     'group_id': '0',  # 组别
     'price': '7.50'  # 价格
 }
 
 PARAMS_AQ_Steril = {
-    'author': '王猪猪',
-    'tel': '18100695026',
-    'address': '江苏省南通市港闸区陈桥街道丽阳花园8号楼',
+    'author': '',
+    'tel': '',
+    'address': '',
     'num': '1',
     'fid': '1888',
     'group_id': '0',
